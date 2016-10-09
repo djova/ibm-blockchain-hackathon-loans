6cab7c0b88df93c7ce59d89013f933ab7d98ce71
Dusan Jovanovic <djovanovic@flipboard.com>
2016-10-09 01:01:19 -0400

    parsing post

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const multer = require('multer')
 const config = require('./config/settings.json')
 const blockchaintest = require('./app/blockchaintest.js')
 const app = express()
 
 console.log(`Starting blockchain test server`)
 
+// globals
 const chain = blockchaintest.initChain()
 
+// middleware
 app.use(morgan('combined'))
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get('/hello', function (req, res) {
-    res.send('GET /hello request to the homepage');
+
+// routes
+
+app.post('/chaincode/deploy', function (req, res) {
+    console.log(`Deploying chaincode: ${req.body.chaincode_name}`)
+    res.send(`Successfully deployed chaincode: ${req.body.chaincode_name}`);
 });
 
 // POST method route
@@ -22,5 +32,3 @@ app.post('/', function (req, res) {
 app.listen(config.serverListenPort, function () {
     console.log(`Example app listening on port ${config.serverListenPort}!`);
 });
-
-
