66efefee62ac127737070f96e3eb837ec38d9900
Dusan Jovanovic <djovanovic@flipboard.com>
2016-10-09 00:53:43 -0400

    updates

--- app/blockchaintest.js
+++ app/blockchaintest.js
@@ -3,6 +3,7 @@ const util = require('util');
 const fs = require('fs');
 const https = require('https');
 const config = require('../config/settings.json')
+const serviceCredentials = JSON.parse(fs.readFileSync(config.serviceCredentialsFile, 'utf8'))
 
 process.env['GRPC_SSL_CIPHER_SUITES'] = 'ECDHE-RSA-AES128-GCM-SHA256:' +
     'ECDHE-RSA-AES128-SHA256:' +
@@ -14,15 +15,14 @@ process.env['GRPC_SSL_CIPHER_SUITES'] = 'ECDHE-RSA-AES128-GCM-SHA256:' +
     'ECDHE-ECDSA-AES256-GCM-SHA384';
 
 
-module.exports.initChain = function() {
+function createAndConnectChain() {
     console.log("Initializing chain")
     //var chain = hfc.newChain("targetChain");
     console.log(`Loading service credentials from ${config.serviceCredentialsFile}`)
-    var servCreds = JSON.parse(fs.readFileSync(config.serviceCredentialsFile, 'utf8'))
     console.log("Loaded service credentials")
 
-    var ca_id = Object.keys(servCreds.ca)[0]
-    var memberServicesUrl = `grpcs://${servCreds.ca[ca_id].url}`
+    var ca_id = Object.keys(serviceCredentials.ca)[0]
+    var memberServicesUrl = `grpcs://${serviceCredentials.ca[ca_id].url}`
     var keystore = `${config.hfcKeystoreDir}/keyValStore-${ca_id}`
 
     var chain = hfc.newChain("targetChain");
@@ -37,38 +37,57 @@ module.exports.initChain = function() {
         pem: pem
     })
 
-    console.log(`Adding ${servCreds.peers.length} peers`)
-    servCreds.peers.forEach(function (peer) {
-        console.log(`Added peer ${peer.id}`)
+    console.log(`Adding ${serviceCredentials.peers.length} peers`)
+    serviceCredentials.peers.forEach(function (peer) {
+        // console.log(`Added peer ${peer.id}`)
         chain.addPeer(`grpcs://${peer.discovery_host}:${peer.discovery_port}`, {
             pem: pem
         })
     })
 
-    chain.enroll("WebAppAdmin", "375d12930a", function(err, webAppAdmin) {
+    return chain
+}
+
+var state = {}
+
+function successfulChainInit(chain, contractUser) {
+    console.log("Chain successfully initialized.")
+    state['chain'] = chain
+    state['contractUser'] = contractUser
+}
+
+/**
+ * Initializes the chain using the local configuration and enrolls the admin user
+ * https://github.com/hyperledger/fabric/tree/master/sdk/node#terminology
+ */
+module.exports.initChain = function() {
+    var chain = createAndConnectChain()
+    var adminUser = serviceCredentials.users.find(user => user.enrollId == config.userEnrollIdToUseForAdmin)
+    var contractUser = serviceCredentials.users.find(user => user.enrollId == config.userEnrollIdToUseForContracts)
+
+    if (adminUser === undefined) {
+        return console.log(`ERROR: Couldn't find admin user '${config.userEnrollIdToUseForAdmin}' in ${config.serviceCredentialsFile}`)
+    }
+    if (contractUser === undefined) {
+        return console.log(`ERROR: Couldn't find contract user '${config.userEnrollIdToUseForContracts}' in ${config.serviceCredentialsFile}`)
+    }
+
+    console.log(`Enrolling admin user: ${adminUser.enrollId}`)
+    chain.enroll(adminUser.enrollId, adminUser.enrollSecret, function(err, user1) {
         if (err) {
-            console.log("ERROR: failed to enroll client: %s",err)
+            console.log(`ERROR: failed to enroll admin user: ${err}`)
         } else {
-            // Successfully enrolled WebAppAdmin during initialization.
-            // Set this user as the chain's registrar which is authorized to register other users.
-            chain.setRegistrar(webAppAdmin)
-            // Now begin listening for web app requests
-            console.log("Registered admin")
+            console.log(`Successfully enrolled admin user ${adminUser.enrollId}`)
+            chain.setRegistrar(user1)
+            chain.enroll(contractUser.enrollId, contractUser.enrollSecret, function(err, user2) {
+                if (err) {
+                    console.log(`ERROR: failed to enroll contract user: ${err}`)
+                } else {
+                    console.log(`Successfully enrolled contract user ${contractUser.enrollId}`)
+                    successfulChainInit(chain, user2)
+                }
+            })
         }
     })
 }
 
-function initChain2(keyStore, memberServiceUr) {
-    var chain = hfc.newChain("targetChain");
-    chain.setKeyValStore(hfc.newFileKeyValStore('/tmp/keyValStore') );
-    chain.setMemberServicesUrl("grpc://MEMBERSRVC_ADDRESS");
-    chain.addPeer("grpc://PEER_ADDRESS");
-    chain.enroll("WebAppAdmin", "DJY27pEnl16d", function(err, webAppAdmin) {
-        if (err) return console.log("ERROR: failed to register %s: %s",err);
-        // Successfully enrolled WebAppAdmin during initialization.
-        // Set this user as the chain's registrar which is authorized to register other users.
-        chain.setRegistrar(webAppAdmin);
-        // Now begin listening for web app requests
-        listenForUserRequests();
-    });
-}
--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const index = require('./app/index')
+const express = require('express')
+const morgan = require('morgan')
 const config = require('./config/settings.json')
 const blockchaintest = require('./app/blockchaintest.js')
+const app = express()
 
-console.log(`Starting blockchain test`)
-blockchaintest.initChain()
+console.log(`Starting blockchain test server`)
+
+const chain = blockchaintest.initChain()
+
+app.use(morgan('combined'))
+
+app.get('/hello', function (req, res) {
+    res.send('GET /hello request to the homepage');
+});
+
+// POST method route
+app.post('/', function (req, res) {
+    res.send('POST request to the homepage');
+});
+
+app.listen(config.serverListenPort, function () {
+    console.log(`Example app listening on port ${config.serverListenPort}!`);
+});
+
+
